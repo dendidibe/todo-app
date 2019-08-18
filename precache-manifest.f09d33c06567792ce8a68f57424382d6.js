e7462ecb84ba8beb620cb5eafb5185775875593d
dendidibe <dendidibe@programm.com>
2019-08-18 23:21:04 +0300

    Updates

---
+++ precache-manifest.f09d33c06567792ce8a68f57424382d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6deced48885c98ceadfc4dd63651757c",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f11ac522a02193a9aaad",
+    "url": "/todo-app/static/css/main.a642740c.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo-app/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "f11ac522a02193a9aaad",
+    "url": "/todo-app/static/js/main.5052b50f.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
