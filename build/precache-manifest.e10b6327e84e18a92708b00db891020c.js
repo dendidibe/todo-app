c19326cbb7be6e95974ffc0dd2daedeb78acceec
dendidibe <mogilyk32@gmail.com>
2020-09-18 22:14:24 +0300

    README

---
+++ build/precache-manifest.e10b6327e84e18a92708b00db891020c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2b14ccd20212ebbe1d3c06f9540ee7",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "699fd38aa3e1fee82da4",
+    "url": "/todo-app/static/css/main.74afd892.chunk.css"
+  },
+  {
+    "revision": "8eff7a9df9e3d927b2c4",
+    "url": "/todo-app/static/js/2.0706156e.chunk.js"
+  },
+  {
+    "revision": "699fd38aa3e1fee82da4",
+    "url": "/todo-app/static/js/main.1ab431bf.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
